4929753877be7d83cf80705b0806b9615c50c529
asohyannick <asohyannick3@gmail.com>
2025-06-15 12:28:33 -0700

    Users and admin can now edit and update their profile accounts successfully  and respectively because the API endpoint has been created

--- src/utils/validators.md.ts
+++ src/utils/validators.md.ts
@@ -36,9 +36,26 @@ const validateProfileRegistration = Yup.object().shape({
 
 });
 
+const validateUpdatedProfileRegistration = Yup.object().shape({
+    firstName: Yup.string().required("FirstName must be provided").trim().min(2),
+    lastName: Yup.string().required("lastName must be provided").trim().min(2),
+    email: Yup.string().email("Email must be provided").required("Email must be provided").trim(),
+    password: Yup.string().required("Password must be provided").trim().min(6, "Password must be at least 6 characters long"),
+    bio: Yup.string().required('Bio must be provided').trim(),
+    profilePicture: Yup.string().required('Profile picture URL must be provided').trim(),
+    profession: Yup.string().required('Profession must be provided').trim(),
+    country: Yup.string().required('Country must be provided').trim(),
+    maritalStatus: Yup.mixed()
+        .required("One value must be provided")
+        .oneOf(Object.values(MaritalStatus)), // Validates against the enum values
+
+});
+
+
 export { 
     validateUserRegistration,
     validateUserLogin,
     validateUserAccountUpdate,
-    validateProfileRegistration
+    validateProfileRegistration,
+    validateUpdatedProfileRegistration
 }
